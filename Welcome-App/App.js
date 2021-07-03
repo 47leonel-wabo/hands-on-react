baa8a1a9fef4362f0a3aa3b7bad9f089e1baa202
leonel47 <waboleonel@gmail.com>
2021-07-03 12:12:28 -0400

    Code refactoring

--- Welcome-App/App.js
+++ Welcome-App/App.js
@@ -2,42 +2,23 @@ import React from 'react';
 import './style.css'
 import Title from './components/Title';
 import Greeting from './components/Greeting';
+import Input from './components/Input';
 
-class App extends React.Component {
+const  App = props => {
 
-    constructor(props){
-        super(props)
-        this.state = {
-            username: 'Mysterious',
-            name: ''
-        }
-    }
+    const [username, setUsername] = React.useState('')
 
-    handleUpdate = () => this.setState({
-        username: this.state.name, 
-        name: ''
-    })
-
-    handleValueChange = ($e) => this.setState({
-        name: $e.target.value
-    })
-
-    render() { 
-        // const [username] = this.state
-        return ( 
-            <>
-                <div className="rt">
-                   <Title title="Welcome to React World" />
-                    <Greeting username={this.state.username} />
-                    <p className="conversation">Tell me what is your name, so we make conversation.</p>
-                    <div className="in-btn">
-                        <input type="text" value={this.state.name} onChange={this.handleValueChange} />
-                        <button onClick={this.handleUpdate} disabled={!this.state.name}>Update name</button>
-                    </div>
-                </div>
-            </>
-         );
-    }
+    return ( 
+        <>
+            <div className="rt">
+                <Title title="Welcome to React World" />
+                <Greeting username={username} />
+                <p className="conversation">Tell me what is your name, so we make conversation.</p>
+                <Input handleUpdate={name => setUsername(name)} />
+            </div>
+        </>
+        );
+    
 }
  
 export default App;
--- Welcome-App/components/Greeting.jsx
+++ Welcome-App/components/Greeting.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Greeting = (props) => <p>Hi there, <strong>{props.username}</strong>!</p>
+const Greeting = (props) => <p>Hi there, <strong>{props.username || 'Mysterious'}</strong>!</p>
 
 export default Greeting
