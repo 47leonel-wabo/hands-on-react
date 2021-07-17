53083e0c33e5ec172f4e04fb0d5a144c398f75a2
leonel47 <waboleonel@gmail.com>
2021-07-16 22:23:43 -0400

    Defining initial base file structure

---
+++ hooks-deep-dive/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import CallBackExple from './CallBackExple'
+// import ContextExple from './ContextExple'
+// import RefExple from './RefExple'
+// import MemoExple from './MemoExple'
+
+import './style.css'
+
+function App(){
+    return (
+        <div class="container">
+            <div class="notification">
+            This container is <strong>centered</strong> on desktop.
+            </div>
+        </div>)
+}
+
+export default App
